492543ae2b98e4c8b8481cb7ae7d8d8f4c0c4eb8
embercraze <maher.shaker@live.se>
2022-07-24 02:50:06 +0200

    change string type to Cow str which include decoding per default according to rocket

--- src/tictactoe/routes.rs
+++ src/tictactoe/routes.rs
@@ -3,20 +3,20 @@ use rocket::serde::json::Json;
 
 
 #[get("/", format = "json", data = "<message>")]
-pub fn index(message: Json<serializers::Message>) -> Option<Json<serializers::Message>> {
+pub fn index(message: Json<serializers::Message<'_>>) -> Option<Json<serializers::Message>> {
     Some(Json(serializers::Message {
             id: message.id+1,
-            message: [&message.message, " Success!"].join(""),
+            message: format!("{}{}", message.message, " Success!").into(),
         }))
     
 }
 
 #[get("/board", format = "json", data = "<game>")]
-pub fn board(game: Json<serializers::Game>) -> Option<Json<serializers::Game>> {
+pub fn board(game: Json<serializers::Game<'_>>) -> Option<Json<serializers::Game>> {
     //TODO we should only get an id and fetch the object based on that
     Some(Json(serializers::Game{
             id: game.id+1,
-            active_player: [&game.active_player, " Success!"].join(""),
+            active_player: [&game.active_player, " Success!"].join("").into(),
             board: game.board.clone(),
         }))
     
--- src/tictactoe/serializers.rs
+++ src/tictactoe/serializers.rs
@@ -1,17 +1,18 @@
+use std::borrow::Cow;
 use rocket::serde::{Deserialize, Serialize};
 
 // If the serialization and decerialization are identical, we can combine them into a single
 // struct. Otherwize use two different structs
 #[derive(Serialize, Deserialize)]
-pub struct Message {
+pub struct Message<'r> {
     pub id: usize,
-    pub message: String,
+    pub message: Cow<'r, str>,
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct Game{
+pub struct Game<'r>{
     pub id: usize,
-    pub active_player: String,
+    pub active_player: Cow<'r, str>,
     // Defines a board that is of size 3x3 full of strings
-    pub board: [[String; 3]; 3],
+    pub board: [[Cow<'r, str>; 3]; 3],
 }
